25764079ccc66c5da39c262adae33487a3446e63
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-07-24 14:42:51 -0300

    Problem Number 13

--- Program.cs
+++ Program.cs
@@ -1,40 +1,49 @@
-﻿//https://leetcode.com/problems/two-sum/
-//Two Sum
+﻿//https://leetcode.com/problems/roman-to-integer
+//Roman To Integer
+/*
+    Neste desafio aprendi melhor como manipular dicionários e laços de repetição;
+    Além de manipulação de strings retirando diretamente os caracteres da string; 
+*/
 
-Solution s = new Solution();
-int[] nums = { 2, 7, 11, 15 };
-int target = 9;
-foreach(int i in s.TwoSum(nums, target))
-{
-    Console.Write(i);
-    Console.Write(",");
-}
+
+// Teste de execução da solução
+Solution sol = new Solution();
+Console.WriteLine(sol.RomanToInt("III"));
 
 public class Solution
 {
+    // Criação do dicionário com a chave sendo o char do numero romano e o valor sendo o numero que ele equivale
+    Dictionary<char, int> numbers = new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
 
-    public int[] TwoSum(int[] nums, int target)
+    // criação do método que transforma um romano em um inteiro
+    public int RomanToInt(string s)
     {
 
-        Dictionary<int, int> map = new Dictionary<int, int>();
-
-
-        for (int i = 0; i <= nums.Length; i++)
+        var result = 0;
+        // laço de repetição que irá correr por cada caractere que existe na string fornecida
+        for (int i = 0; i < s.Length; i++)
         {
-            // 
-            int x = target - nums[i];
-            if (map.ContainsKey(x))
+            // lógica que verifica se o numero romano atual é menor do que o proximo numero romano, para verificar se são um numero só;
+            if (i < s.Length - 1 && numbers[s[i]] < numbers[s[i + 1]])
             {
-                return new int[] { map[x], i };
+                // Adiciona ao resultado o valor do proximo numero e subtrai pelo numero atual da sequencia, gerando assim o numero real que representa o romano
+                result += numbers[s[i + 1]] - numbers[s[i]];
+                i++;
             }
-
-            if (!map.ContainsKey(nums[i]))
-            {
-                map[nums[i]] = i;
-            }
+            else
+                // se o numero nao se enquadra na lógica ele é somado ao resultado final
+                result += numbers[s[i]];
         }
-        return new int[0];
+
+        return result;
     }
-
-
 }
