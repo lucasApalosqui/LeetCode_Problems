aaa3f743c9bfff97fd006fc3cf1c4b0a856b8b77
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-01-02 10:35:15 -0300

    Exercise - Log Levels

--- Program.cs
+++ Program.cs
@@ -1,49 +1,29 @@
-﻿//https://leetcode.com/problems/roman-to-integer
-//Roman To Integer
-/*
-    Neste desafio aprendi melhor como manipular dicionários e laços de repetição;
-    Além de manipulação de strings retirando diretamente os caracteres da string; 
-*/
-
-
-// Teste de execução da solução
-Solution sol = new Solution();
-Console.WriteLine(sol.RomanToInt("III"));
-
-public class Solution
+﻿public static class LogLine
 {
-    // Criação do dicionário com a chave sendo o char do numero romano e o valor sendo o numero que ele equivale
-    Dictionary<char, int> numbers = new Dictionary<char, int>()
-        {
-            {'I', 1 },
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-
-    // criação do método que transforma um romano em um inteiro
-    public int RomanToInt(string s)
+    public static string Message(string logLine)
     {
+        var split = logLine.Split(":");
+        var message = split[1];
+        return message.Trim();
 
-        var result = 0;
-        // laço de repetição que irá correr por cada caractere que existe na string fornecida
-        for (int i = 0; i < s.Length; i++)
-        {
-            // lógica que verifica se o numero romano atual é menor do que o proximo numero romano, para verificar se são um numero só;
-            if (i < s.Length - 1 && numbers[s[i]] < numbers[s[i + 1]])
-            {
-                // Adiciona ao resultado o valor do proximo numero e subtrai pelo numero atual da sequencia, gerando assim o numero real que representa o romano
-                result += numbers[s[i + 1]] - numbers[s[i]];
-                i++;
-            }
-            else
-                // se o numero nao se enquadra na lógica ele é somado ao resultado final
-                result += numbers[s[i]];
-        }
+    }
 
-        return result;
+    public static string LogLevel(string logLine)
+    {
+        var split = logLine.Split(":");
+        var log = split[0];
+        return log.Trim(['[', ']']).ToLower();
+    }
+
+    public static string Reformat(string logLine)
+    {
+        var split = logLine.Split(":");
+        var message = split[1];
+        var log = split[0];
+
+        message = message.Trim();
+        return $"{message} ({log.Trim(['[', ']']).ToLower()})";
     }
 }
+
+
