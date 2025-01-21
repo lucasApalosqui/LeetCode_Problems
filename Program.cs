d81248b3ca0d719db4383a2f65c396f046b2ff99
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-01-21 12:46:04 -0300

    Exercism - Tracks on Tracks on Tracks

--- Program.cs
+++ Program.cs
@@ -1,21 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 
-
-Console.WriteLine(Badge.Print(null, "Amare Osei", null));
-static class Badge
+public static class Languages
 {
-    public static string Print(int? id, string name, string? department)
+    private static List<string> _languages = new List<string>() { "C#", "Clojure", "Elm" };
+
+    public static List<string> NewList()
     {
-        if(id != null && department != null)
-            return $"[{id}] - {name} - {department.ToUpper()}";
+        return new List<string>();
+    }
 
-        if (id == null && department != null)
-            return $"{name} - {department.ToUpper()}";
+    public static List<string> GetExistingLanguages()
+    {
+        return _languages;
+    }
 
-        if (department == null)
-            department = "OWNER";
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        if (language != null) languages.Add(language);
+        return languages;
+    }
 
-        return (id == null) ? $"{name} - {department.ToUpper()}" : $"[{id}] - {name} - {department.ToUpper()}";
+    public static int CountLanguages(List<string> languages)
+    {
+        return languages.Count;
+    }
 
+    public static bool HasLanguage(List<string> languages, string language)
+    {
+        return (languages.Contains(language) ? true : false);
+    }
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        languages.Reverse();
+        return languages;
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        if(languages.Count == 0 || !languages.Contains("C#"))
+            return false;
+
+        if (languages[0] == "C#")
+            return true;
+        
+        return (languages[1] == "C#" && languages.Count >= 2 && languages.Count <= 3) ? true : false;
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        if(!languages.Contains(language))
+            return languages;
+
+        languages.Remove(language);
+        return languages;
+    }
+
+    public static bool IsUnique(List<string> languages)
+    {
+        foreach (string language in languages)
+        {
+            var languagecont = 0;
+            for (int i = 0; i < languages.Count; i++)
+            {
+                if (languages[i] == language) languagecont++;
+
+
+            }
+            if (languagecont >= 2) return false;
+        }
+        return true;
     }
 }
