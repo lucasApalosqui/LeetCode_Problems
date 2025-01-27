7c35182585f4df53df19c9d890170fce97761925
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-01-27 10:29:42 -0300

    Exercism - Football Match Reports

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
-public class Player
-{
-    public int RollDie()
-    {
-        return new Random().Next(1, 18);
-    }
+using System.Runtime.CompilerServices;
 
-    public double GenerateSpellStrength()
+public static class PlayAnalyzer
+{
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch
     {
-        return new Random().NextDouble() * (100.0 - 0.0) + 0.0;
-    }
+        1 => "goalie",
+        2 => "left back",
+        3 or 4 => "center back",
+        5 => "right back",
+        6 or 7 or 8 => "midfielder",
+        9 => "left wing",
+        10 => "striker",
+        11 => "right wing",
+        _ => "UNKNOWN"
+    };
+
+    public static string AnalyzeOffField(object report) => report switch
+    {
+        int sup => $"There are {sup} supporters at the match.",
+        string announ => announ,
+        Injury injury => $"Oh no! {injury.GetDescription()} Medics are on the field.",
+        Incident incident => incident.GetDescription(),
+        Manager manager => (manager.Club is null) ? manager.Name : $"{manager.Name} ({manager.Club})",
+        _ => ""
+    };
 }
