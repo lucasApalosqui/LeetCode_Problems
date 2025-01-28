d9865c09c36d1ab8bcfeb30642cb54148c1af6e2
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2025-01-28 11:58:04 -0300

    Exercism - Attack of the Trolls

--- Program.cs
+++ Program.cs
@@ -1,129 +1,43 @@
 ﻿using System;
-using System.Data;
-using System.Globalization;
-using System.Runtime.InteropServices;
 
-
-
-public enum Location
+public enum AccountType
 {
-    NewYork,
-    London,
-    Paris
+    Guest,
+    User,
+    Moderator
 }
 
-public enum AlertLevel
+[Flags]
+public enum Permission : byte
 {
-    Early,
-    Standard,
-    Late
+    Read = 1 << 0,
+    Write = 1 << 1,
+    Delete = 1 << 2,
+    All = Write | Delete | Read,
+    None = 0
 }
 
-public static class Appointment
+
+static class Permissions
 {
-    public static string _osx = GetSystem(RuntimeInformation.OSDescription);
-    public static DateTime ShowLocalTime(DateTime dtUtc)
+    public static Permission Default(AccountType accountType) => accountType switch
     {
-        return dtUtc.ToLocalTime();
-    }
-
-    public static DateTime Schedule(string appointmentDateDescription, Location location) => location switch
-    {
-        Location.NewYork => TransformToNewUtc(appointmentDateDescription, GetZone(location, _osx)),
-        Location.Paris => TransformToNewUtc(appointmentDateDescription, GetZone(location, _osx)),
-        Location.London => TransformToNewUtc(appointmentDateDescription, GetZone(location, _osx)),
-        _ => DateTime.MinValue
-
-
+        AccountType.Guest => Permission.Read,
+        AccountType.User => Permission.Write | Permission.Read,
+        AccountType.Moderator => Permission.All,
+        _ => Permission.None
     };
-
-    public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel) => alertLevel switch
+    public static Permission Grant(Permission current, Permission grant)
     {
-        AlertLevel.Early => appointment.AddDays(-1),
-        AlertLevel.Standard => appointment.AddHours(-1).AddMinutes(-45),
-        AlertLevel.Late => appointment.AddMinutes(-30),
-        _ => appointment
-    };
-
-    public static bool HasDaylightSavingChanged(DateTime dt, Location location)
+        return current | grant;
+    }
+    public static Permission Revoke(Permission current, Permission revoke)
     {
-        var dateParameter = dt.AddDays(-7);
-        TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(GetZone(location, _osx));
-        return tzi.IsDaylightSavingTime(dateParameter) != tzi.IsDaylightSavingTime(dt);
+        return current & ~revoke;
     }
 
-    public static DateTime NormalizeDateTime(string dtStr, Location location)
+    public static bool Check(Permission current, Permission check)
     {
-        try
-        {
-            return DateTime.Parse(dtStr, LocationToCulture(location));
-
-        }
-        catch
-        {
-            return DateTime.MinValue;
-        }
-    }
-
-
-    
-    //Utils
-    private static DateTime TransformToNewUtc(string date, string zone)
-    {
-        try
-        {
-            DateTime utc = DateTime.Parse(date);
-            TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(zone);
-
-            return TimeZoneInfo.ConvertTimeToUtc(utc, tzi);
-        }
-        catch
-        {
-            return DateTime.MinValue;
-        }
-    }
-
-    private static string GetSystem(string osx) => osx switch
-    {
-        string os when os.Contains("Windows") => "Windows",
-        string os when os.Contains("Linux") => "Linux",
-        string os when os.Contains("OSX") => "OSX",
-        _ => ""
-    };
-
-    private static string GetZone(Location location, string osx)
-    {
-        if (location == Location.NewYork)
-        {
-            return (osx == "Windows") ? "Eastern Standard Time" : "America/New_York";
-        }
-        if (location == Location.London)
-        {
-            return (osx == "Windows") ? "GMT Standard Time" : "Europe/London";
-        }
-        if (location == Location.Paris)
-        {
-            return (osx == "Windows") ? "W. Europe Standard Time" : "Europe/Paris";
-        }
-        else
-            return "";
-    }
-
-    private static CultureInfo LocationToCulture(Location location)
-    {
-        string cultureId = string.Empty;
-        switch (location)
-        {
-            case Location.NewYork:
-                cultureId = "en-US";
-                break;
-            case Location.London:
-                cultureId = "en-GB";
-                break;
-            case Location.Paris:
-                cultureId = "fr-FR";
-                break;
-        }
-        return new CultureInfo(cultureId);
+        return current.HasFlag(check);
     }
 }
